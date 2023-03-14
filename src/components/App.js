0ddd57b6c3f5a0335357ae4126b80ea9ae61044b
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-03-14 17:25:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from 'react'
 import '../styles/App.css';
-import { useLocation } from 'react-router-dom';
 
 const App = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const id = queryParams.get('id');
-  const name = queryParams.get('name');
-  const designation = queryParams.get('designation');
-
+  //Write your code here
+  const queryParams = new URLSearchParams(window.location.search)
+  const id = queryParams.get("id")
+  const name = queryParams.get("name")
+  const designation = queryParams.get("designation")
+  
   return (
-    <div className="details">
-      Employee {id} named {name} works as {designation}
+    <div className="App">
+      <div className="details">
+        <p>Employee {id} named {name} works as {designation}</p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
